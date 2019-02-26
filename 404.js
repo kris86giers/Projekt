c2996de328e21eb07d1d5df74bacb6c4f7ab9d5b
kris86giers <48012396+kris86giers@users.noreply.github.com>
2019-02-26 11:02:55 +0100

    Delete 404.js

--- 404.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const rootDir = require('../util/path');
-exports.get404 = (req, res, next ) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404page.html'));
-};
